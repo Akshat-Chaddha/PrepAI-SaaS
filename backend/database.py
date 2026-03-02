3734a0e818b4cd36a115aef0256bbffbf8fa3f6d
Akshat091205 <23ad10ak9@mitsgwl.ac.in>
2026-03-02 19:48:47 +0530

    Add dashboard page

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,10 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = os.getenv("postgresql://prepai_db_wn2m_user:wS9FCPQHg1vCLsyJWRcRNAZfDtT0gGD5@dpg-d6in0aruibrs73ae8mag-a.singapore-postgres.render.com/prepai_db_wn2m")
+DATABASE_URL = os.getenv(postgresql://prepai_db_wn2m_user:wS9FCPQHg1vCLsyJWRcRNAZfDtT0gGD5@dpg-d6in0aruibrs73ae8mag-a/prepai_db_wn2m)
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL is not set in environment variables")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
